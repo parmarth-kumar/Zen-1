60caddb17a6cc45edda8ed6fdc5b6af8d1f9ffd1
Trishita Ghosh <ghoshtrishita44@gmail.com>
2025-10-04 18:26:48 +0530

    Update CategoryFilter.tsx

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -17,4 +17,42 @@ return(
   <div className="realtive inline-block">
     {/*--- Glassy Main Button ---*/}
     <button
+      onClick ={()=> setIsOpen(!isOpen)}
+      className="flex items-center space-x-2 px-3 py-1.5
+       bg-gray-900/40 backdrop-blur-lg
+       border border-gray-900/60
+       rounded-xl cursor-pointer hover:bg-gray-900/60
+       transition-all shadow-md"
+      >
+      {/*Left-side filter icon for clarity*/}
+      <Filter className ="w-4 h-4 text-gray-300"/>
+
+      {/* Display the selected category name, or fallback if none*/}
+      <span className ="text-sm font-medium text-white">
+        {selectedCategoryData?.name || 'All Research'}
+      </span>
+      {/* Chevron rotates when dropdown is open*/}
+      <ChevronDown
+        className={`w-4 h-4 text-gray-300 transition-transform ${isOpen ? 'rotate-180' : ''}`}
+        />
+      </button>
+
+    {/*--- Dropdown Menu (conditionally rendered if open)---*/}
+    
+      {isOpen && (
+          <div
+            className="absolute top-full right-0 mt-2 w-52 
+            bg-gray-900/40 
+            backdrop-blur-lg
+            border border-gray/20 rounded-xl shadow-xl z-50"
+            >
+               <div className="p1">
+                 {/* mao over all categories to show them as dropdown option*/}
+                 {categories.map(category => (
+                       <button
+                         key={category.id}
+                         onClick={() => {
+                           onCategoryChange(category.id);//notify parent of category selection 
+                           setIsOpen(false): //close dropdown
+                         }}
 
