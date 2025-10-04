135d5182f7b28efde572cbd5416f7fd794e6dfcd
Vansh Baranwal <168327605+Vansh-Baranwal@users.noreply.github.com>
2025-10-04 16:50:31 +0530

    Import Libraries

--- src/components/ResearchModal.tsx
+++ src/components/ResearchModal.tsx
@@ -1,1 +1,5 @@
+import React from 'react';
+//Add lightbulb
+import { X, Mapin, Timer, User, Sparkles, ExternalLink, Lightbulb } from 'lucide-react';
+import { ResearchItem, categories } from '../data/research-data';
 
