beba4271491ecc3b97c1f8c7fb8ff3c37b302462
Parmarth Kumar <parmarthk26@gmail.com>
2025-10-04 13:57:49 +0530

    module imports in process-with-ai.js
    Figured out approach and imported required modules.

--- process-with-ai.js
+++ process-with-ai.js
@@ -1,1 +1,12 @@
+// APPROACH
+// get title and link from NASA data csv file
+// get publication contents
+// summarize it using ai
+// store data in json file
 
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import fs from 'fs';
+import Papa from 'papaparse';
+import { GoogleGenerativeAI} from '@google/generative-ai';
+import 'detenv/config';
